f2b00c3971a75529d0e22318f7f0ad837d713d1d
Valentin Kraft <valentin.kraft@online.de>
2018-05-02 15:31:02 +0200

    removed computer/pixel shader plugin from dependencies

--- Source/GPUPointCloudRenderer/GPUPointCloudRenderer.Build.cs
+++ Source/GPUPointCloudRenderer/GPUPointCloudRenderer.Build.cs
@@ -27,7 +27,7 @@ namespace UnrealBuildTool.Rules
             PrivateIncludePaths.Add("GPUPointCloudRenderer/Private");
             PublicIncludePaths.Add("GPUPointCloudRenderer/Public");
 
-            PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "ComputeShader", "PixelShader" });
+            PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI" });
             PrivateDependencyModuleNames.AddRange(new string[] { "Core", "Projects" });
 
         }
